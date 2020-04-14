45c002f5cd0ad69a6c61bc18ebbc2c106be1edb8
dedoor <57615920+dedoor@users.noreply.github.com>
2020-04-14 15:51:03 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
-<html>
-    <h1>Login</h1>
-    </br>Matrikelnummer
-</html>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
