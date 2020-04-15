052d5e4ce33361cfb413e3926f19b402fc436005
f4ntus <johannes@familie-scheffold.de>
2020-04-15 13:30:06 +0200

    Add SqlWrapper Class

---
+++ SqlWrapper.php
@@ -0,0 +1,25 @@
+<?php
+    class SqlWrapper{
+        const DBHOST = "localhost";
+        const DBUSER = "root";
+        const DBPASSWORD = "";
+        const DATABASE = "befragungstool";
+        private $db;
+        
+        public function __construct()
+        {
+            $this->db = new MySQLi(self::DBHOST, self::DBUSER, self::DBPASSWORD, self::DATABASE);
+        }
+        
+        public function insertIntoBefrager($benutzername,$passwort){
+            $sql = "INSERT INTO tbl_befrager (Benutzername, Kennwort) VALUES ('$benutzername', '$kennwort')";
+
+            if ($this->db->query($sql)) {
+                return true;
+            } else {
+                echo "Error: " . $sql . "<br>" . $this->db->error;
+            }
+        }
+
+    }
+?>
