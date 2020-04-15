b16d91923169ada9c1404766388c2c05b2ecc030
dedoor <57615920+dedoor@users.noreply.github.com>
2020-04-15 10:47:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,37 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
+<!DOCTYPE html>
+<body> <html>
+<form method="post" action="http://vorlesungen.kirchbergnet.de/inhalte/DB-PR/output_posted_vars.php">
+  <h1>Login</h1>
+    </br>
+    Benutzername: 
+        <input type="text" name="benutername">
+            </br>
+            </br>
+    Passwort: 
+        <input type="password" name="passwort" required>   
+            </br>
+            </br>
+  
+  <input type="submit" name="anmelden" value="Anmelden"/>
+  <input type="submit" name="registrieren" value="Registrieren"/>
+            </br>
+            </br>
 
-// Create connection
-$conn = new mysqli($servername, $username, $password);
+    <?php
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-?>
+            // Create connection
+            $conn = new mysqli($servername, $username, $password);
+
+            // Check connection
+         if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+            }
+        echo "Connected successfully";
+    ?>
+</form>
+
+</body> </html>
+
