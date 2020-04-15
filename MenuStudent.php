e64e8bef10dde467b0ca665ab9427e9c3bb0c2ee
LS3-sydatlo <LS3-sydatlo@users.noreply.github.com>
2020-04-15 17:50:47 +0200

    Test

---
+++ MenuStudent.php
@@ -0,0 +1,20 @@
+<html> 
+
+
+
+<head>
+<title>MenüStudent</title>
+</head>
+<body>
+
+<?php
+
+echo selectFragebogen();
+
+/* while($ausgabe = $this->sqlWrapper->selectFragebogen()->fetch_object()) {
+        echo $ausgabe->FbNr;
+    }
+*/
+?>
+</body>
+</html>
