bead382f1f99fcf6828659ae2056722ffc468910
LS3-sydatlo <LS3-sydatlo@users.noreply.github.com>
2020-04-16 12:47:39 +0200

    Fragebogenausgabe

--- MenuStudent.php
+++ MenuStudent.php
@@ -1,7 +1,4 @@
 <html> 
-
-
-
 <head>
 <title>MenüStudent</title>
 </head>
@@ -9,12 +6,32 @@
 
 <?php
 
-echo selectFragebogen();
 
-/* while($ausgabe = $this->sqlWrapper->selectFragebogen()->fetch_object()) {
-        echo $ausgabe->FbNr;
-    }
-*/
+$host = "localhost"; 
+$user= "root";
+$password = "";
+$db = "befragungstool";
+$db = new MySQLi($host, $user, $password, $db);
+
+$q = $db->query("SELECT * FROM tbl_fragebogen");
+
+
+echo "<table><tr><th>FragebogenNr</th><th>Titel</th>";
+
+while($row = $q->fetch_assoc()) { 
+    echo "<tr>";
+    echo "<td>" . $row['FbNr'] . "</td>";
+    echo "<td>" . $row['Titel'] . "</td>";
+    echo "</tr>";
+
+} 
+
+echo "</table>"
+
+
+
+
+
 ?>
 </body>
 </html>
