9873b025128294d3099747c4d416bd4b8f754f0c
f4ntus <johannes@familie-scheffold.de>
2020-05-13 19:03:36 +0200

    Add other Tabels as Classes

---
+++ Model/tbl_Kommentiert.php
@@ -0,0 +1,32 @@
+<?php
+
+require 'AbstractSQLWrapper.php';
+class TblKommentiert extends AbstractSQLWrapper
+{
+    function selectUniqueRecord()
+    {
+        $sql = '';
+        return $this->globalSelectUniqueRecord($sql);
+    }
+    function selectRecords()
+    {
+        $sql = '';
+        return $this->globalSelectRecords($sql);
+    }
+    function updateRecord()
+    {
+        $sql = '';
+        return $this->globalUpdateRecord($sql);
+    }
+    function insertRecord()
+    {
+        $sql = '';
+        return $this->globalInsertRecord($sql);
+    }
+    function deleteRecord()
+    {
+        $sql = '';
+        return $this->globalDeleteRecord($sql);
+    }
+}
+?>
